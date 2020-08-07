301dba8e18aaaf890be6e6d20e3dc22272944bef
elandau91 <eli.b.landau@gmail.com>
2020-08-07 15:20:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * Infinity)
+
+const mod = 20 % 8
+
+const max = Math.max(1, 6, 20)
